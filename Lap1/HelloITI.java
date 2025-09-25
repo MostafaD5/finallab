7b443750ed57ab1d9ea6b9a43d12871e68953daf
MostafaD <136201384+MostafaD5@users.noreply.github.com>
2025-09-25 12:36:37 +0300

    Add files via upload

---
+++ Lap1/HelloITI.java
@@ -0,0 +1,9 @@
+public class HelloITI {
+    public void printHelloITI() {
+        System.out.println("Hello ITI");
+    }
+
+    public void printHelloGit() {
+        System.out.println("Hello Git");
+    }
+}
